3f920aa234f24e4b81311c7206de4475889f3b75
jjaison26 <jj89177@mail.harpercollege.edu>
2024-10-02 16:42:02 -0500

    Update file 3
    created geometric swapper class

--- 3_friend_classes.cpp
+++ 3_friend_classes.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class GeometricScaler;
-
+class GeometricSwapper;
 class Rectangle
 {
   int width, height;
@@ -15,7 +15,14 @@ public:
   {
     return (width * height);
   }
+  int getWidth() {
+      return this->width;
+  }
+  int getHeight() {
+      return this->height;
+  }
   void scale(GeometricScaler scalar);
+  void swap(GeometricSwapper swapper);
 };
 
 class GeometricScaler
@@ -30,11 +37,24 @@ public:
     this->factor = factor;
   }
 };
+class GeometricSwapper {
+    private:
+    friend class Rectangle;
+    int width;
+    int height;
+};
 
 void Rectangle::scale(GeometricScaler scalar)
 {
   this->width = this->width * scalar.factor;
   this->height = this->height * scalar.factor;
+}
+
+void Rectangle::swap(GeometricSwapper swapper) {
+    swapper.width = this->height;
+    swapper.height = this->width;
+    this->width = swapper.width;
+    this->height = swapper.height;
 }
 
 int main()
@@ -46,6 +66,11 @@ int main()
   rectangle.scale(scaler);
 
   cout << "Area of the Rectangle: " << rectangle.area() << endl;
+  
+  GeometricSwapper swapper;
+  cout << "Width and Height of Rectangle before : " << rectangle.getWidth() << ", " << rectangle.getHeight() << endl;
+  rectangle.swap(swapper);
+  cout << "Width and Height of Rectangle after : " << rectangle.getWidth() << ", " << rectangle.getHeight() << endl;
 
   return 0;
 }
